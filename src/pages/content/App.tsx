ae42f5fee4d1a09a9d2c258a1287ca81505ebde0
Arhun Saday <arhunsad@gmail.com>
2025-01-27 10:31:59 +0100

    chore: init from template

---
+++ src/pages/content/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="fixed bottom-0 w-screen left-0 text-lg text-black bg-amber-400 z-50">
+      <App />
+    </div>
+  );
+}
---
+++ src/pages/content/index.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import { App } from "./App";
+const div = document.createElement("div");
+div.id = "__root";
+document.body.appendChild(div);
+
+const rootContainer = document.querySelector("#__root");
+if (!rootContainer) throw new Error("Can't find Content root element");
+const root = createRoot(rootContainer);
+root.render(<App />);
+
+try {
+  console.log("content script loaded");
+} catch (e) {
+  console.error(e);
+}
